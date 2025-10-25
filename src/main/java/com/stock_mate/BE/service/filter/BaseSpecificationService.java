45e34a047f67e8279bf586ed0fb6a8830465a091
mngdinh <minhdc.uni@gmail.com>
2025-10-25 14:44:11 +0700

    Update BaseSpecificationService.java

--- src/main/java/com/stock_mate/BE/service/filter/BaseSpecificationService.java
+++ src/main/java/com/stock_mate/BE/service/filter/BaseSpecificationService.java
@@ -11,7 +11,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.function.Function;
-import java.util.function.Predicate;
 
 public abstract class BaseSpecificationService<T, R> {
 
@@ -22,12 +21,16 @@ public abstract class BaseSpecificationService<T, R> {
     /**
      * Mỗi service con sẽ implement logic filter riêng.
      */
-    protected Specification<T> buildSpecification(String searchTerm) {
+    protected abstract Specification<T> buildSpecification(String searchTerm);
+
+    /**
+     * Hàm mặc định - tự động thêm điều kiện filter theo ngày nếu searchTerm là dd-MM-yyyy
+     */
+    protected Specification<T> buildDateSpecification(String searchTerm) {
         return (root, query, cb) -> {
             if (searchTerm == null || searchTerm.trim().isEmpty()) {
                 return cb.conjunction();
             }
-
             if (isDateFormat(searchTerm)) {
                 try {
                     SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
@@ -44,13 +47,11 @@ public abstract class BaseSpecificationService<T, R> {
                     calendar.set(Calendar.MINUTE, 59);
                     calendar.set(Calendar.SECOND, 59);
                     Date endDate = calendar.getTime();
-
                     return cb.or(
                             cb.between(root.get("createDate"), startDate, endDate),
                             cb.between(root.get("updateDate"), startDate, endDate)
                     );
                 } catch (ParseException e) {
-                    // Nếu không parse được, bỏ qua
                     return cb.conjunction();
                 }
             }
@@ -60,7 +61,12 @@ public abstract class BaseSpecificationService<T, R> {
 
     public Page<R> getAll(String search, int page, int size, String[]  sort) {
 
-        Specification<T> spec = buildSpecification(search);
+        Specification<T> spec;
+        if (isDateFormat(search)) {
+            spec = buildDateSpecification(search);
+        } else {
+            spec = buildSpecification(search);
+        }
 
         String sortField = sort[0];
         String sortDirection = sort[1];
