1749ba1b492e567e35955e039e8f149c901fbad7
mngdinh <minhdc.uni@gmail.com>
2025-10-25 11:31:44 +0700

    Update RawMaterialRequest.java

--- src/main/java/com/stock_mate/BE/dto/Request/RawMaterialRequest.java
+++ src/main/java/com/stock_mate/BE/dto/Request/RawMaterialRequest.java
@@ -1,16 +1,12 @@
 package com.stock_mate.BE.dto.request;
 
 import com.stock_mate.BE.enums.RawMaterialCategory;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-public class RawMaterialRequest {
-    String name;
-    String code;
-    String description;
-    RawMaterialCategory category;
-    String dimension;
-    Integer thickness;
-}
+public record RawMaterialRequest(
+        String name,
+        String code,
+        String description,
+        RawMaterialCategory category,
+        String dimension,
+        Integer thickness
+) {}
