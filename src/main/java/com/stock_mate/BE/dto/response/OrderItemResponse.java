e9ad48fdc63652845d93148cd37fdf0e1fc54e86
mngdinh <minhdc.uni@gmail.com>
2025-11-08 08:25:57 +0700

    Remove materialID from OrderItem and related DTOs
    Eliminated the materialID field from OrderItem entity, request and response DTOs, and updated OrderService to reflect this change. This simplifies the data model and removes unused or redundant information.

--- src/main/java/com/stock_mate/BE/dto/response/OrderItemResponse.java
+++ src/main/java/com/stock_mate/BE/dto/response/OrderItemResponse.java
@@ -3,7 +3,6 @@ package com.stock_mate.BE.dto.response;
 
 public record OrderItemResponse(
         int itemID,
-        String materialID,
         int quantity,
         FinishProductResponse finishProduct
 ) {
